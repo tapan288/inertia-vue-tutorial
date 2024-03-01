212085d42be27d1928b62329552374ff4732f0a0
Tapan Sharma <tapansharma69@gmail.com>
2024-03-01 10:23:46 +0545

    setup models/migrations/factories/seeders/relationships

---
+++ app/Models/Student.php
@@ -0,0 +1,28 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Student extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'name',
+        'email',
+        'class_id',
+        'section_id'
+    ];
+
+    public function class ()
+    {
+        return $this->belongsTo(Classes::class, 'class_id');
+    }
+
+    public function section()
+    {
+        return $this->belongsTo(Section::class, 'section_id');
+    }
+}
